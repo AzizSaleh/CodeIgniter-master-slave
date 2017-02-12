50be738360e0114e3f863df4780b8833916a20ba
Aziz Saleh <azizsaleh@gmail.com>
2017-02-12 14:16:09 -0500

    Update config
    Update config

--- application/config/database.php
+++ application/config/database.php
@@ -80,7 +80,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | The $query_builder variables lets you determine whether or not to load
 | the query builder class.
 */
-//$active_group = 'master'; // Not needed, MY_Model.php handles it
+$active_group = 'master';
 $query_builder = TRUE;
 
 /*
